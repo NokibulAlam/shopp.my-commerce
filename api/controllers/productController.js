be642f1dcca5f91c44116b4e4746ac8d62837bf6
NokibulAlam <nokibul.alam02@gmail.com>
2022-09-30 01:35:18 +0600

    product update & delete module added

--- api/controllers/productController.js
+++ api/controllers/productController.js
@@ -1,10 +1,26 @@
 const Product = require('../models/Product');
 const formadable = require('formidable'); // For Handling Image Upload; Checking if the user uploading a valid file
 const fs = require('fs');
+const _lodash = require('lodash'); // For Saving Previous Values
 
 /* Internal Import */
 const {errorHandler} = require('../helper/dbErrorHandle');
 
+
+// Fetch Product ID
+exports.productById = (req, res, next, id) => {
+    Product.findById(id)
+        .populate("category")
+        .exec((err, product) => {
+            if(err || !product) {
+                return res.status(400).json({
+                    error: "Product Not Found"
+                });
+            }
+            req.product = product;
+            next();
+        });
+};
 
 
 // Create Product
@@ -55,5 +71,65 @@ exports.create = (req, res, next) => {
                 return res.json(result);
             }
         });
+    });
+};
+
+
+// Update Product
+exports.update = (req, res, next) => {
+    // Form's All Information will store at thi variable
+    const form = new formadable.IncomingForm();
+
+    form.keepExtensions = true;
+
+    form.parse(req, (err, fields, files) => {
+        if(err){
+            return res.status(400).json({
+                error: "Image upload Error!"
+            });
+        }
+
+        let product = req.product;
+        product = _lodash.extend(product, fields); // Current Product and Fields Given by USER
+
+        if(files.photo) {
+            if(files.photo.size > 1000000) {
+                return res.status(400).json({
+                    error: "Photo size should be less than 1MB"
+                });
+            }
+
+            // Photo read with Syncronize way and Save the Value to Data Colmn 
+            product.photo.data = fs.readFileSync(files.photo.path);
+            product.photo.contentType = files.photo.type; // Will Save File types
+
+        }
+        product.save((err, result) => {
+            if(err){
+                return res.status(400).json({
+                    error: errorHandler(err),
+                });
+            }
+            else {
+                return res.json(result);
+            }
+        });
+    });
+};
+
+
+// product Delete Module
+exports.delete = (req, res, next) => {
+    const product = req.product;
+
+    product.remove((err, result) => {
+        if(err) {
+            return res.status(400).json({
+                error: errorHandler(err),
+            });
+        }
+        else {
+            return res.json({message: "Product Deleted Successfully"});
+        }
     });
 };
--- api/routes/productRoute.js
+++ api/routes/productRoute.js
@@ -12,8 +12,17 @@ router.route('/product/create/:userId')
     .post(authController.requireSignIn, authController.isAuth, authController.isAdmin, productController.create);
 
 
+// Product Update Route
+router.route('/product/:productId/:userId')
+    .put(authController.requireSignIn, authController.isAuth, authController.isAdmin, productController.update)
+    .delete(authController.requireSignIn, authController.isAuth, authController.isAdmin, productController.delete)
+
+// Find Product by ID
+router.param('productId', productController.productById);
 
 // Find User by ID
 router.param('userId', userController.userById);
+
+
 
 module.exports = router;
